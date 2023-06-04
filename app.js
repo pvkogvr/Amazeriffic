10a1116f4f683b66c7b8898a019d18771bcaabba
pvkog <87erumog@gmail.com>
2023-06-04 15:31:34 +0300

    Commit posle polgogo prostoya. Obrati vnimanie na nth-child(3)

--- app.js
+++ app.js
@@ -25,7 +25,47 @@ var main = function(toDoObjects){
                 toDos.forEach(function (todo){
                     $(".content").append($("<li>").text(todo));
                 });
-            } else if ($element.parent().is(":nth-child(3)")){
+            } else if($element.parent().is(":nth-child(3)")){
+                console.log("Щелчек на вкладку теги");
+                // var organizedByTag = [
+                //     {
+                //         "name":"покупки",
+                //         "toDos":["Купить продукты"]
+                //     },
+                //     {
+                //         "name":"рутина",
+                //         "toDos":["Купить продукты","Вывести Грейси на прогулку в парк"]
+                //     },
+                //     {
+                //         "name":"писательство",
+                //         "toDos":["Сделать несколько новых задач","Закончить писать книгу"]
+                //     },
+                //     {
+                //         "name":"работа",
+                //         "toDos":["Сделать несколько новых задач","Подготовиться к лекции в понедельник",
+                //         "Ответить на электронные письма","Закончить писать книгу"]
+                //     },
+                //     {
+                //         "name":"преподавание",
+                //         "toDos":["Подготовиться к лекции в понедельник"]
+                //     },
+                //     {
+                //         "name":"питомцы",
+                //         "toDos":["Вывести Грейси на прогулку в парк"]
+                //     }
+                // ]
+                var organizedByTag = organizedByTags(toDoObjects)
+                organizedByTag.forEach(function(tag){
+                    var $tagName = $("<h3>").text(tag.name);
+                    $content = $("<ul>");
+                    tag.toDos.forEach(function(description){
+                        var $li = $("<li>").text(description);
+                        $content.append($li);
+                    })
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                })
+            }else if ($element.parent().is(":nth-child(4)")){
                 var newTodo;
                 $(".content").append($("<input>")).on("keydown", function(e){
                     if(e.keyCode == 13){
